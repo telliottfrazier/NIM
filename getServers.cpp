764adee58eb45bd760ffa0221c64f5f5e935e06d
jcurnutt14 <38479502+jcurnutt14@users.noreply.github.com>
2018-04-23 17:28:13 -0500

    updates

--- getServers.cpp
+++ getServers.cpp
@@ -20,11 +20,11 @@ using namespace std;
 //							  contain the name, IP Address and port number of a remote TicTacToe server.
 //	numServers				= Number of elements in serverArray[]
 
-//	Function return value	= Number of remote TicTacToe servers (size of serverArray[])
+//	Function return value	= Number of remote NIM servers (size of serverArray[])
 
 int getServers(SOCKET s, char *broadcastAddress, char *broadcastPort, ServerStruct serverArray[])
 {
-	// This function returns the number of TicTacToe servers that may be found within the current Broadcast Domain.
+	// This function returns the number of NIM servers that may be found within the current Broadcast Domain.
 	// The last parameter, serverArray[], will contain all of the servers' names, IP addresses and port numbers.
 
 	int numServers = 0;
@@ -52,27 +52,22 @@ int getServers(SOCKET s, char *broadcastAddress, char *broadcastPort, ServerStru
 		std::cin >> challengeName;
 		char *challenge;
 		strcat(NIM_CHALLENGE, challengeName);
-		int numBytesSent = UDP_send(s, challenge, strlen(challenge) + 1, broadcastAddress, NIM_UDPPORT);
+		int numBytesSent = UDP_send(s, challenge, strlen(challenge) + 1, host, NIM_UDPPORT);
 
 		int status2 = wait(s, 15, 0);
 
-		int numBytesRecv = UDP_recv(s, recvBuffer, sizeof(recvBuffer) - 1, host, port);
+		char recvBuffer2[MAX_RECV_BUF + 1];
 
-		if (strcmp(recvBuffer, "Yes") != 0) 
+		int numBytesRecv = UDP_recv(s, recvBuffer2, sizeof(recvBuffer2) - 1, host, port);
+
+		if (strcmp(recvBuffer2, "Yes") == 0) 
 		{
 
+			int numBytesSent = UDP_send(s, NIM_ACCEPT, strlen(NIM_ACCEPT) + 1, host, NIM_UDPPORT);
+
 			string name;
-			while (status > 0 && len > 0)
+			while (status2 > 0 && len > 0)
 			{
-				/****
-				Task 4b: Inside this while loop, extract a response, which should be a C-string that looks like "Name=some server's name".
-				If the response doesn't begin with the characters, "Name=", ignore it.
-				If it does begin with the characters, "Name=", parse the actual name that follows and
-				(i) assign that name to the array of structs, serverArray[numServers].name
-				(ii) assign the IP Address from which the response originated to serverArray[numServers].host
-				(iii) assign the server's port number to serverArray[numServers].port
-				(iv) increment numServers
-				****/
 
 				string recvStr(recvBuffer);
 				if (recvStr.substr(0, 5) == "Name=")
